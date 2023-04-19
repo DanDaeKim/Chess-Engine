1fd17cc59f632177357740f499581580ff0ee280
DanDaeKim <131207269+DanDaeKim@users.noreply.github.com>
2023-04-19 16:03:37 -0700

    Update search_algo.py

--- search_algo.py
+++ search_algo.py
@@ -4,10 +4,58 @@ class ChessEngine:
         self.time_manager = time_manager
         self.transposition_table = {}
         
+    def make_move(self):
+        num_moves_left = self.estimate_moves_left()
+        time_for_move = self.time_manager.time_for_move(num_moves_left)
+        best_move = self.find_best_move(max_depth, time_limit=time_for_move)
+        self.board.make_move(best_move)
+        
+    def estimate_moves_left(self):
+    num_pieces, num_pawns, material_balance = self.analyze_position()
+    phase_factor = self.calculate_phase_factor(num_pieces, num_pawns, material_balance)
+
+    if phase_factor < 0.3:
+        return 50  # Opening phase
+    elif phase_factor < 0.7:
+        return 35  # Middle game
+    else:
+        return 20  # Endgame
+
+    def analyze_position(self):
+        num_pieces = 0
+        num_pawns = 0
+        material_balance = 0
+
+        piece_values = {'P': 1, 'p': -1, 'N': 3, 'n': -3, 'B': 3, 'b': -3,
+                        'R': 5, 'r': -5, 'Q': 9, 'q': -9, 'K': 0, 'k': 0}
+
+        for row in self.board.board:
+            for piece in row:
+                if piece != '-':
+                    num_pieces += 1
+                    material_balance += piece_values[piece]
+
+                    if piece.upper() == 'P':
+                        num_pawns += 1
+
+        return num_pieces, num_pawns, material_balance
+
+    def calculate_phase_factor(self, num_pieces, num_pawns, material_balance):
+        max_pieces = 32
+        max_pawns = 16
+        max_material_balance = 72  # 2 knights, 2 bishops, 2 rooks, 1 queen, 8 pawns for each side
+
+        piece_factor = num_pieces / max_pieces
+        pawn_factor = num_pawns / max_pawns
+        material_factor = (material_balance + max_material_balance) / (2 * max_material_balance)
+
+        return (piece_factor + pawn_factor + material_factor) / 3
+
+
     def order_moves(self, moves):
-    # Sort moves based on a simple heuristic or previous search results
-    # For example, you can sort the moves based on captures first
-    return sorted(moves, key=lambda move: self.board.is_capture(move), reverse=True)
+        # Sort moves based on a simple heuristic or previous search results
+        # For example, you can sort the moves based on captures first
+        return sorted(moves, key=lambda move: self.board.is_capture(move), reverse=True)
 
 
     def alpha_beta_search(self, depth, alpha, beta, maximizing_player):
