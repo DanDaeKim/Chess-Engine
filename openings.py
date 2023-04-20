fd5e43bc11b28e76732a8eb9b83c97bb9e02232f
DanDaeKim <131207269+DanDaeKim@users.noreply.github.com>
2023-04-19 18:17:34 -0700

    Create openings.py

---
+++ openings.py
@@ -0,0 +1,11 @@
+# opening_book.py
+opening_book = {
+    'rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1': [('e2', 'e4'), ('d2', 'd4'), ('g1', 'f3'), ('c2', 'c4')],
+    'rnbqkbnr/pppp1ppp/8/4p3/4P3/8/PPPP1PPP/RNBQKBNR w KQkq e6 0 2': [('d2', 'd4'), ('g1', 'f3')],
+    'rnbqkbnr/pppp1ppp/8/4p3/3P4/8/PPP2PPP/RNBQKBNR b KQkq d3 0 2': [('d7', 'd6'), ('e5', 'd4'), ('b8', 'c6'), ('g8', 'f6')],
+    'rnbqkb1r/pppp1ppp/5n2/4p3/3P4/5N2/PPP2PPP/RNBQKB1R w KQkq - 0 3': [('d4', 'e5'), ('b1', 'c3'), ('c1', 'g5'), ('e1', 'g1')],
+    'rnbqkb1r/pppp1ppp/5n2/4P3/8/5N2/PPP2PPP/RNBQKB1R b KQkq - 0 3': [('f6', 'd5'), ('f6', 'e4')],
+    'r1bqkb1r/pppp1ppp/2n2n2/4p3/2PP4/2N5/PP3PPP/R1BQKBNR w KQkq - 0 5': [('g1', 'f3'), ('d1', 'c2'), ('c1', 'g5')],
+    'r1bqkb1r/pppp1ppp/2n2n2/4p3/2PP4/2N2N2/PP3PPP/R1BQKB1R b KQkq - 0 5': [('e5', 'd4'), ('b8', 'b4')],
+    # ... Add more FEN strings and their corresponding recommended moves here
+}
